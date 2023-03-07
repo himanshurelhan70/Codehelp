7417310324959145ee5363b99fc200fed5f9b82a
himanshurelhan70 <himanshurelh70@gmail.com>
2023-03-07 13:58:34 +0530

    deleted extra profile share modal

---
+++ Projects/MyPortfolioWebsite/js/script.js
@@ -0,0 +1,12 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Web Developer",
+      "Frontend Developer",
+      "Full Stack Developer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
