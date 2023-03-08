d900865cb69fad9fc3c1bf394dea74bb8f8cba53
himanshurelhan70 <himanshurelh70@gmail.com>
2023-03-08 13:30:17 +0530

    making portfolio responsive

--- Projects/MyPortfolioWebsite/js/script.js
+++ Projects/MyPortfolioWebsite/js/script.js
@@ -1,3 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', () => {
+  mobileNav.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
+
+
 var typeData = new Typed(".role", {
     strings: [
       "Web Developer",
