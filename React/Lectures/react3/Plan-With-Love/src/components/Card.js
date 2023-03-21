8610c4b653456c603b20cc7227bdb9542bd62a05
himanshurelhan70 <himanshurelh70@gmail.com>
2023-03-21 13:27:03 +0530

    added react 3

---
+++ React/Lectures/react3/Plan-With-Love/src/components/Card.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+
+function Card({id, image,info,price,name, removeTour})  {
+    const[readmore,setReadmore] = useState(false);
+    
+    const description = readmore ? info :`${info.substring(0,200)}....`;
+    function readmoreHandler() {
+        setReadmore(!readmore);
+    }
+
+
+    return (
+        
+        <div className="card">
+
+            <img src={image} className="image"></img>
+
+            <div className="tour-info">
+                <div className="tour-details">
+                    <h4 className="tour-price">₹ {price}</h4>
+                    <h4 className="tour-name">{name}</h4>
+                </div>
+
+                <div className="description">
+                    {description}
+                    <span className="read-more" onClick={readmoreHandler}>
+                        {readmore ? `Show Less`: `Read \More`}
+                    </span>
+                </div>
+            </div>
+
+            <button className="btn-red" onClick={() => removeTour(id)}>
+                Not Interested
+            </button>
+        </div>
+        
+    );
+}
+
+export default Card;
---
+++ React/Lectures/react3/Plan-With-Love/src/components/Tours.js
@@ -0,0 +1,19 @@
+import Card from './Card'
+function Tours({tours, removeTour}) {
+    return (
+        <div className='container'>
+            <div>
+                <h2 className='title'> Plan With Love</h2>
+            </div>
+            <div className='cards'>
+                {
+                    tours.map( (tour) => {
+                        return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
+                    } )
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Tours;
