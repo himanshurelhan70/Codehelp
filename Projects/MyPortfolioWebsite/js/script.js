e9e660e51daa6c21cdeaa11c70a04d5a447e4f8b
himanshurelhan70 <himanshurelh70@gmail.com>
2023-03-25 01:49:51 +0530

    added responsiveness to portfolio

--- Projects/MyPortfolioWebsite/js/script.js
+++ Projects/MyPortfolioWebsite/js/script.js
@@ -1,10 +1,20 @@
 const hamburger = document.querySelector('.hamburger');
 const mobileNav = document.querySelector('.mobile-nav');
+const overlay = document.querySelector('.overlay');
 
 hamburger.addEventListener('click', () => {
   mobileNav.classList.toggle('active');
   hamburger.classList.toggle('active');
+  overlay.classList.toggle('active');
 });
+
+function closeMobileNav(){
+  mobileNav.classList.remove('active');
+  hamburger.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+overlay.addEventListener("click", closeMobileNav);
 
 
 var typeData = new Typed(".role", {
