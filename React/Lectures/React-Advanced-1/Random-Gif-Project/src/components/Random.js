bca45ff62733f90096dc6eabc033aefb04d4a89e
himanshurelhan70 <himanshurelh70@gmail.com>
2023-03-30 16:50:15 +0530

    created random GIF project

---
+++ React/Lectures/React-Advanced-1/Random-Gif-Project/src/components/Random.js
@@ -0,0 +1,39 @@
+// import React, { useState } from 'react'
+// import axios from 'axios';
+// import { useEffect } from 'react';
+import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
+
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+const Random = () => {
+
+
+  const {gif, loading, fetchData} = useGif();
+
+
+  return (
+    <div className='lg:w-1/2 w-full bg-green-500 rounded-xl border border-black
+    flex flex-col items-center gap-y-5 mt-[15px]'>
+
+      <h1 className='mt-[15px] text-xl md:text-2xl underline uppercase font-bold'> A Random Gif</h1>
+
+    {
+        loading ? (<Spinner/>) : (<img src= {gif} width="450" />)
+    }
+
+      
+
+      <button onClick={() => fetchData()}
+      className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px] uppercase  font-medium tracking-wide">
+
+       Generate
+
+      </button>
+
+    </div>
+  )
+}
+
+export default Random
---
+++ React/Lectures/React-Advanced-1/Random-Gif-Project/src/hooks/useGif.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+
+
+const useGif = (tag) => {
+    const [gif, setGif] = useState('');
+    const [loading, setLoading] = useState('false');
+
+  
+    async function fetchData(tag) {
+      setLoading(true);
+      
+      const {data} = await axios.get(tag ? `${url}&tag=${tag}`  : url);
+      const imageSource = data.data.images.downsized_large.url;
+      setGif(imageSource);
+      setLoading(false);
+    }
+    
+    useEffect( () => {
+      fetchData('car');
+    },[] )
+
+    return {gif,loading, fetchData};
+}
+
+export default useGif
