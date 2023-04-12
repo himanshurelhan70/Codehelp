1e6f6a5b3909059c8bbcfd4c463bd45f2fb2a0d9
himanshurelhan70 <himanshurelh70@gmail.com>
2023-04-12 13:42:33 +0530

    created shopping cart project

---
+++ React/Lectures/React-Advanced-5/Shopping-Cart-Project/src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import Spinner from "../components/Spinner";
+import Product from "../components/Product";
+
+const Home = () => {
+  const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  async function fetchProductData() {
+    setLoading(true);
+
+    try{
+      const res = await fetch(API_URL);
+      const data = await res.json();
+
+      setPosts(data);
+    }
+    catch(error) {
+      console.log("Error aagya ji");
+      setPosts([]);
+    }
+    setLoading(false);
+  }
+
+  useEffect( () => {
+    fetchProductData();
+  },[])
+
+  return (
+    <div>
+      {
+        loading ? <Spinner />  :
+        posts.length > 0 ? 
+        (<div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5
+        gap-y-8 max-w-6xl p-6 mx-auto my-7 min-h-[80vh]">
+          {
+            posts.map( (post) => (
+            <Product key = {post.id} post={post}/>
+          ) )
+          }
+        </div>) :
+        <div className="w-screen h-screen flex justify-center items-center">
+          <p className="font-bold">No Data Found</p>
+        </div> 
+      }
+    </div>
+  );
+};
+
+export default Home;
