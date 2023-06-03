82be772f29e159931f42ab428acaf1ae25b39d2b
himanshurelhan70 <himanshurelh70@gmail.com>
2023-06-03 19:47:38 +0530

    added CSS projects

--- Projects/CSS/12 - ImageGalleryGrid/js/script.js
+++
@@ -1,7 +0,0 @@
-const captions = document.getElementsByClassName("caption");
-
-console.log(captions);
-
-Array.from(captions).forEach((caption, index) => {
-    caption.innerText = `Image ${index+1}`;
-});
